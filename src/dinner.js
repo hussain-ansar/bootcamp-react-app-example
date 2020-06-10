e9345e9d71fe02c3579bdc1f4672c65916e3357f
Ansar Hussain <65576958+hussain-ansar@users.noreply.github.com>
2020-06-10 19:02:25 +0500

    Update dinner.js

--- src/dinner.js
+++ src/dinner.js
@@ -4,7 +4,7 @@ function dinner(props) {
     return(
         <div>
         <h1>Today we are serving {props.dishName}</h1>
-        <h1>Today we are serving Sweet Dish {props.sweetDish}</h1>
+        <h1>Today we are also serving Sweet Dish {props.sweetDish}</h1>
         </div>
     );
 }
